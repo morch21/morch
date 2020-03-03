f4ee3aa3638a8eb04e64d562a13d281a91c83866
Mikhail Shchegolev <mshchego@e1z3r7p9.42.us.org>
2020-03-02 19:06:36 -0800

    added my functions

--- libft.h
+++ libft.h
@@ -84,5 +84,8 @@ void				ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+void				ft_print_s(char *s, char *str);
+void				ft_print_i(char *s, int i);
+void				ft_print_arr(char **s);
 
 #endif
